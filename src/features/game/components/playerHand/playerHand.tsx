a45926030b24cea65a838b278e9c4c575dd31f8c
Gaël Motte <gael.motte@bonjour.io>
2020-04-15 09:12:24 +0200

    :heavy_plus_sign: Adding styles-components

--- src/features/game/components/playerHand/playerHand.tsx
+++ src/features/game/components/playerHand/playerHand.tsx
@@ -4,15 +4,17 @@ import { useSelector, useDispatch } from "react-redux";
 import { selectPlayerHand } from "../../gameSlice";
 import Card, { compareValues } from "../card/Card";
 
+import StyledHand from "./playerHand.style";
+
 export function PlayerHand() {
   let hand = useSelector(selectPlayerHand);
   return (
-    <>
+    <StyledHand>
       {hand &&
         hand
           .slice()
           .sort(compareValues)
           .map((cardId) => <Card cardIndex={cardId} key={cardId}></Card>)}
-    </>
+    </StyledHand>
   );
 }
