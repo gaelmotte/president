bc827166700d00eee25796b9f3a1decf499c074c
Gaël Motte <gael.motte@bonjour.io>
2020-04-17 20:48:39 +0200

    :lipstick: card style

--- src/features/game/components/card/Card.tsx
+++ src/features/game/components/card/Card.tsx
@@ -4,12 +4,15 @@ import { useSelector, useDispatch } from "react-redux";
 import { selectPlayerHand } from "../../gameSlice";
 
 import { getFigure, getColor } from "../../../../services/cardsUtils";
+import StyledCard from "./Card.style";
 
 export default ({ cardIndex }: { cardIndex: number }) => {
   return (
-    <div>
-      {getFigure(cardIndex)}
-      {getColor(cardIndex)}
-    </div>
+    <StyledCard>
+      <div className={getColor(cardIndex)}>
+        {getFigure(cardIndex)}
+        {getColor(cardIndex)}
+      </div>
+    </StyledCard>
   );
 };
