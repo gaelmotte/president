a63867427f5a2729f871ffa23b938645caaad394
Gaël Motte <gael.motte@bonjour.io>
2020-04-19 12:24:03 +0200

    :construction: init card exchange comp

---
+++ src/features/game/components/cardExchanges/cardExchanges.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useRef } from "react";
+import classNames from "classnames";
+
+import Card from "../card/Card";
+
+import StyledCardExchanges from "./cardExchanges.style";
+
+export default () => {
+  return (
+    <StyledCardExchanges>
+      <h2>Card Exchange Comp</h2>
+    </StyledCardExchanges>
+  );
+};
