0bb6b1622cea99309d1eb677f72ccb9bf95f6857
Gaël Motte <gael.motte@bonjour.io>
2020-05-08 20:52:42 +0200

    :bug: inoput fixes

--- src/features/room/Room.style.ts
+++ src/features/room/Room.style.ts
@@ -21,13 +21,14 @@ export default styled.div<{ valid: boolean }>`
       text-decoration: none;
       background-color: black;
       border: 0;
-      font-size: 2vmin;
+
       color: white;
     }
 
     input[type="submit"] {
       box-shadow: inset 0 0 1vmin ${(props) => (props.valid ? "gold" : "red")};
       border-radius: 1vmin;
+      font-size: 7vmin;
     }
   }
 `;
