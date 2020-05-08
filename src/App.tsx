a201dfabd0c69986b444b26c5e1fc74254ac565e
Gaël Motte <gael.motte@bonjour.io>
2020-05-08 18:49:40 +0200

    :wheelchair: force landscape

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,26 @@ import React from "react";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import useIsMobile from "./hooks/useIsMobile";
+
 import { Room } from "./features/room/Room";
 import { Home } from "./features/home/Home";
 
 import "./App.css";
 
 function App() {
+  const isMobile = useIsMobile();
   return (
     <div className="App">
+      {isMobile && (
+        <div className="force-landscape">
+          <img
+            className="rotate"
+            src="/rotate.gif"
+            alt="Please Use Landscape Orientation"
+          />
+        </div>
+      )}
       <Router>
         <Switch>
           <Route path="/:roomId">
